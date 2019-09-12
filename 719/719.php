8b9aa9b9f4b85276f0ecb93b2e5f7609dde472b5
foostart.com <foostart.com@gmail.com>
2019-09-12 14:21:55 +0700

    test

---
+++ 719/719.php
@@ -0,0 +1,37 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    $less = new lessc;
+    $less->compileFile('less/719.less', 'css/719.css');
+    
+?>
+<!DOCTYPE html>
+<html style="background-color: #f3f3f4;">
+    <head>
+        <title>Aurum - Fashion</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/719.css" rel="stylesheet" type="text/css" />
+        <script src="<?php echo $url_path ?>/js/jquery.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/scripts.js"></script>
+    </head>
+    <body>
+       <?php include $dir_block.'/719-content.php'; ?>
+        
+    </div>
+</body>
+</html>
