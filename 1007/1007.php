820d374ade674b2e23c7dd4eb2b4f570b136542b
Nguyễn Lê Thành Công <nguyenlethanhcong.10k13@gmail.com>
2019-09-24 11:29:02 +0700

    edit 1007

--- 1007/1007.php
+++ 1007/1007.php
@@ -1,47 +1,65 @@
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Document</title>
 
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/styles.less', 'css/styles.css');
-        ?>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-        <link rel="stylesheet" type="text/css" href="js/bootstrap.min.js"/>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
 
-    </head>
-    <body>
-        <div class="type-17">
-            <div class="container">
-                <!--HERDER-->
-                <div class="herder">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <!--TITLE--->
-                            <h3 class="title">Apple iPhone 6s Plus</h3>
-                        </div>
-                        <div class="col-md-5">
-                            <!--NAV-->
-                            <div class="b-breadcrumbs pull-right hidden-xs ">
-                                <ul class="list-unstyled">
-                                    <li><a href="#">Home></a></li>
-                                    <li><a href="#">Accessories ></a></li>
-                                    <li><a style="text-decoration: none;color: #888888; " href="#">Apple iPhone 6s Plus</a></li>
-                                </ul>
+    <?php
+    if (!class_exists('lessc')) {
+        include('./libs/lessc.inc.php');
+    }
+    $less = new lessc;
+    $less->compileFile('less/styles.less', 'css/styles.css');
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <div class="type-17">
+        <div class="container">
+            <!--HERDER-->
+            <div class="herder">
+                <div class="row">
+                    <div class="col-md-7">
+                        <!--TITLE--->
+                        <h3 class="title">Apple iPhone 6s Plus</h3>
+                    </div>
+                    <div class="col-md-5">
+                        <!--NAV-->
+                        <nav class="navbar navbar-inverse navbar-default affix" data-spy="affix" data-offset-top="50">
+                            <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
                             </div>
-                        </div>
+                                <div id="navbar" class="navbar-collapse collapse">
+                                    <ul class="nav navbar-nav">
+                                        <li><a href="#">HOME &gt;</a></li>
+                                        <li class="dropdown mega-dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">ACCESSORIES &gt;</a>
+                                        </li>
+                                        <li><a href="#page" style="color: gray;">APPLE IPHONE 6 PLUS</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-                <!--END HERDER-->
             </div>
+            <!--END HERDER-->
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
